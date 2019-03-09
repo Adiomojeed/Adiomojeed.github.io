7de8c6ae499e8c58256216c4a57d9f4ba08cd751
Adiomojeed <46828146+Adiomojeed@users.noreply.github.com>
2019-03-09 23:11:48 +0100

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,6 @@
+function read(){
+    var text=document.getElementById("anintro")
+    text.innerHTML="Hi, It's Mojeed here, A full stack web Developer focused on developing complex web projects and startups. Thanks to practice and experience. I produce\n" +
+        "            responsive webpages, marketing pages, blogs, etc. <br> <br>My jobs are qualified, time-efficient and I enjoy solving problems\n" +
+        "                I am ready to collaborate with you to produce maximum output.<br><br> My core technologies are Javascript and PHP."
+}
