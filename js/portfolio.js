86d79e9bd74a831dd5f38b8a379856f1306b0a26
Adiomojeed <adio.mojeed@gmail.com>
2019-05-21 22:32:49 +0100

    rebuilded

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+function change1() {
+    document.getElementById("img").src = "111.png";
+}
+function change2() {
+    document.getElementById("img").src = "clement-h-544786-unsplash.jpg";
+}
+function change3() {
+    document.getElementById("img").src = "kendall-ruth-602587-unsplash.jpg";
+}
+function change4() {
+    document.getElementById("img").src = "112.png";
+}
+function change(color){
+    document.getElementById("intro").style.color = color;
+}
