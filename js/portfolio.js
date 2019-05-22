4db71f6e218d14215cbfb1b62cd6802250cd5fa0
Adiomojeed <adio.mojeed@gmail.com>
2019-05-22 13:31:30 +0100

    re-modified

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,9 +5,6 @@ function change2() {
     document.getElementById("img").src = "clement-h-544786-unsplash.jpg";
 }
 function change3() {
-    document.getElementById("img").src = "kendall-ruth-602587-unsplash.jpg";
-}
-function change4() {
     document.getElementById("img").src = "112.png";
 }
 function change(color){
