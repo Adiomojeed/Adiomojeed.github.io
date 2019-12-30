bb7912a264d72372db0a8f9cf5e885f26dd6d50e
Adiomojeed <adio.mojeed@gmail.com>
2019-12-30 22:49:21 +0100

    added countdown page

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+function change1() {
+    document.getElementById("img").src = "111.png";
+}
+function change2() {
+    document.getElementById("img").src = "clement-h-544786-unsplash.jpg";
+}
+function change3() {
+    document.getElementById("img").src = "112.png";
+}
+function change(color){
+    document.getElementById("intro").style.color = color;
+}
